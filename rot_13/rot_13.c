f810cc2f29534b1b186d2b41dc4dd7dc6d20e35e
MariglenCullhaj <mariglen.cullhaj@gmail.com>
2026-01-16 23:06:17 +0100

    ulstr

--- rot_13/rot_13.c
+++ rot_13/rot_13.c
@@ -1,33 +1,31 @@
 #include <unistd.h>
 
-int main(int argc, char **argv){
-    if(argc != 2){
-        write(1,"\n",1);
+int main(int argc, char **argv)
+{
+    int i = 0;
+    char c;
+
+    if (argc != 2)
+    {
+        write(1, "\n", 1);
         return 0;
     }
-    
-    int i = 0;
 
     while (argv[1][i])
-    {   
-        if (argv[1][i] <= 122 && argv[1][i] >= 109){ // z122 m109
-            char temp = argv[1][i] - 13;
-            write(1,&temp,1);
-            i++;
-        } else if (argv[1][i] >= 97 && argv[1][i] <= 109){ //a97 m109
-            char temp = argv[1][i] + 13;
-            write(1,&temp,1);
-            i++;
-        } else if (argv[1][i] <= 90 && argv[1][i] >= 77){ //Z90 M77
-            char temp = argv[1][i] - 13;
-            write(1,&temp,1);
-            i++;
-        } else if (argv[1][i] >= 65 && argv[1][i] <= 77){ // A65 M77
-            char temp = argv[1][i] +13;
-            write(1,&temp,1);
-            i++;
-        }
+    {
+        c = argv[1][i];
+        if (c >= 'a' && c <= 'm')
+            c += 13;
+        else if (c >= 'n' && c <= 'z')
+            c -= 13;
+        else if (c >= 'A' && c <= 'M')
+            c += 13;
+        else if (c >= 'N' && c <= 'Z')
+            c -= 13;
+
+        write(1, &c, 1);
+        i++;
     }
-    
+    write(1, "\n", 1);
     return 0;
 }
---
+++ ulstr/ulstr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        write(1,"\n",1);
+        return 0;
+    }
+    int i =0;
+    while(argv[1][i])
+    {
+        char c = argv[1][i];
+        if(c >= 'A' && c <='Z' )
+            c +=32;
+        else if(c >= 'a' && c <='z' )
+            c -=32;
+        write(1,&c,1);
+        i++;
+    }
+    write(1,"\n",1);
+    return 0;
+}
