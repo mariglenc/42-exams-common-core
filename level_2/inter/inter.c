d873153125280044c0cd68e18174a07cd3d1cdb3
MariglenCullhaj <mariglen.cullhaj@gmail.com>
2026-01-28 00:07:01 +0100

    inter tesgt

--- level_2/inter/inter.c
+++ level_2/inter/inter.c
@@ -1,60 +1,68 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int check_char_not_exist(char *res, char c)
 {
-    
-    int index_1 = 0;
-    int index_2;
+	int i = 0;
 
-    int seen;
-    int found;
-
-    if (argc != 3)
-    {
-        write(1, "\n", 1);
-        return (0);
-    }
-
-    char *string_1 = argv[1];
-    char *string_2 = argv[2];
-    while (string_1[index_1])
-    {
-        index_2 = 0;
-        found = 0;
-
-        /* check if char exists in argv[2] */
-        while (string_2[index_2])
-        {
-            if (string_1[index_1] == string_2[index_2])
-            {
-                found = 1;
-                break;
-            }
-            index_2++;
-        }
-
-        /* check if already printed */
-        seen = 0;
-        while (seen < index_1 && string_1[seen] != string_1[index_1])
-            seen++;
-
-        if (found && seen == index_1)
-            write(1, &string_1[index_1], 1);
-
-        index_1++;
-    }
-
-    write(1, "\n", 1);
-    return (0);
+	while (res[i]) // "c"
+	{
+		if (res[i] == c)
+			return 1;
+		i++;
+	}
+	return 0;
 }
 
+int check_char_exist(char *str1, char *str2)
+{
+	int i = 0;
 
+	while (str1[i] || str2[i]) // "c"
+	{
+		if (str1[i] == str2[i])
+			return 1;
+		i++;
+	}
+	return 0;
+}
 
-// ./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
-//      padinto$
-// ./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
-//      df6ewg4$
-// ./inter "rien" "cette phrase ne cache rien" | cat -e
-//      rien$
-// ./inter | cat -e
-// $
+int main(int argc, char **argv)
+{
+	int i;
+	int j = 0;
+	char res[256];
+
+	if (argc == 3)
+	{
+		res[0] = '\0';
+
+		i = 0;
+		while (argv[1][i])
+		{
+			if (!check_char_not_exist(res, argv[1][i]))
+			{
+				res[j] = argv[1][i];
+				j++;
+				res[j] = '\0';
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+
+		i = 0;
+		while (argv[2][i])
+		{
+			if (!check_char_not_exist(res, argv[2][i]))
+			{
+				res[j] = argv[2][i];
+				j++;
+				res[j] = '\0';
+				write(1, &argv[2][i], 1);
+			}
+			i++;
+		}
+	}
+
+	write(1, "\n", 1);
+	return 0;
+}
