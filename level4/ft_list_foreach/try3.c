4e55d7af0cfe59929f34d744dfd4acfc84893611
Mariglen Cullhaj <mcullhaj43@gmail.com>
2026-04-09 21:01:50 +0200

    list foreach and list size

--- level4/ft_list_foreach/try3.c
+++ level4/ft_list_foreach/try3.c
@@ -2,15 +2,10 @@
 
 void ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
+
     while (begin_list)
     {
         f(begin_list->data);
         begin_list = begin_list->next;
     }
 }
-
-/*
-1-Iterate on begin_list
-2-on each iteration execute f function on begin_list->data
-3-on each iteration move to the next node begin_list -> next
-*/
